ffc696dde6f216b8253f6d68e7f19487fc0b41d2
JIE <312129271@qq.com>
2017-12-18 21:18:12 +0800

    tiaozhan2

--- calculator.py
+++ calculator.py
@@ -1,53 +1,51 @@
 #!/usr/bin/env python3
-def f(c, gdict={}):
-    b= n - n*0.165 - c
-    i=l1[0]
-    gdict[i] = b
-    for key,value in gdict.items():
-        print(key,":",end='')
-        print(format(value,".2f"))
-l1dict = {}
+#jiangshurugongzibianchengzidian
+def nashuie(b, dict1=None):
+    if dict1 is None:
+        dict1 = {}
+    for b1 in b:
+        dict1[b1.split(':')[0]] = b1.split(':')[1]
+#    print(dict1)
+#tiqugongzi,bingjisuan nashuie
+    for key1,value1 in dict1.items():
+        try:
+            b2 = int(value1)
+        except:
+            print('Parameter Error')
+        dict1[key1] = b2 - b2*0.165 -3500
+#    print(dict1)
+    return dict1
+
+#jisuannashuie
+def nashui(c):
+    if c <= 0:
+        c1 = 0
+    if 0< c <= 1500:
+        c1 = c*0.03
+    if 1500 < c <= 4500:
+        c1 = c*0.1 - 105
+    if 4500 < c <= 9000:
+        c1 = c*0.2 - 555
+    if 9000 < c <= 35000:
+        c1 = c*0.25 - 1005
+    if 35000 < c <= 55000:
+        c1 = c*0.30 - 2755
+    if 55000 < c <= 80000:
+        c1 = c*0.35 - 5505
+    if c > 80000:
+        c1 = c*0.45 - 13505
+    c2 = c - c1 + 3500
+    return c2
+#    print(c1)
+
 import sys
-for a1 in sys.argv[1:]:
-    l1 = a1.split(':')
-#    print(l1[0])
-    l1dict[l1[0]] = l1[1]
-#    print(l1dict)
-for key in l1dict.keys():
-    n1 = key
-#    print(key)
-try:
-    n = int(l1dict[n1])
-except ValueError:
-    print("Paramenter Error")
-a = n - n*0.165 - 3500
-#print(a)
-if a <= 1500:
-    c = a*3/100
-#    print(format(c,".2f"))
-if 1500 < a <= 4500:
-    c = a*10/100 - 105
-#    print(format(c,".2f"))
-if 4500 < a <= 9000:
-    c = a*20/100 - 555
-#    print(format(c,".2f"))
-if 9000 < a <= 35000:
-    c = a*25/100 - 1005
-#    print(format(c,".2f"))
-if 35000 < a <= 55000:
-    c = a*30/100 - 2755
-#    print(format(c,".2f"))
-if 55000 < a <= 80000:
-    c = a*35/100 - 5505
-#    print(format(c,".2f"))
-if a > 80000:
-    c = a*45/100 -13505
-#    print(format(c,".2f"))
-#f(c,gdict={})
-b= n - n*0.165 - c
-l1dict[key] = format(b,".2f")
-#print(l1dict)
-for key,value in l1dict.items():
-#    print(key,value)
-    print('%s%s%s' % (key,':',value))
-#    print(key.join(format(value,".2f")))
+a = sys.argv[1:]
+a1 = nashuie(a)
+#a3 = nashui(13200.0)
+#print(a1)
+for key,value in a1.items():
+    a1[key] = nashui(value)
+#    print(a1)
+print(a1)
+for key2,value2 in a1.items():
+    print('%s%s%s' % (key2,':',"%.2f"%value2))
